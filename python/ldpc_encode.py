4c6e67b09396e6e24eebd43ea5e9b1b1be38ef8b
Veniamin Sukhochev <140503338+benySuho@users.noreply.github.com>
2024-10-27 04:08:10 +0200

    Add files via upload

--- python/ldpc_encode.py
+++ python/ldpc_encode.py
@@ -11,26 +11,25 @@ def shift_vector_by_k(vector, k):
 
 def encode(p_matrix, block_size, message):
     m, n = p_matrix.shape
-    message = np.array(message.reshape(-1))
+    message = np.array(message).reshape(-1)
     codeword = np.append(message, np.zeros(m * block_size, dtype=int))
 
-    #  Double diagonal encoding
     temp = np.zeros(block_size, dtype=int)
     for i in range(m):  # rows
-        for j in range(n - m):  # columns
-            message_part = message[j * block_size:(j + 1) * block_size]
+        for j in range(n - m - 1, -1, -1):  # columns
             shift = p_matrix[i][j]
-            shifted = shift_vector_by_k(message_part, shift)
+            code_part = codeword[j * block_size:(j + 1) * block_size]
+            shifted = shift_vector_by_k(code_part, shift)
             temp = np.mod(temp + shifted, 2)
-
-    codeword[(n - m) * block_size:(n - m + 1) * block_size] = shift_vector_by_k(temp, block_size)
+    codeword[(n - m) * block_size:(n - m + 1) * block_size] = temp
 
     for i in range(m - 1):
         temp = np.zeros(block_size, dtype=int)
-        for j in range(n - m + i + 1):
-            code_part = codeword[j * block_size:(j + 1) * block_size]
+        for j in range(n - 1, -1, -1):
             shift = p_matrix[i][j]
-            temp = np.mod(temp + shift_vector_by_k(code_part, shift), 2)
-        codeword[(n - m + i+1) * block_size:(n - m + i + 2) * block_size] = temp
+            code_part = codeword[j * block_size:(j + 1) * block_size]
+            shifted = shift_vector_by_k(code_part, shift)
+            temp = np.mod(temp + shifted, 2)
+        codeword[(n - m + i + 1) * block_size:(n - m + i + 2) * block_size] = temp
 
     return codeword.reshape(1, -1)
